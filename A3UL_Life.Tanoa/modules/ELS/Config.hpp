be3445d33568dc0b70c182d343bc952e0938ad70
Cameron D <camerondebono@icloud.com>
2026-06-16 13:42:21 -0400

    Update Config.hpp

--- A3UL_Life.Tanoa/modules/ELS/Config.hpp
+++ A3UL_Life.Tanoa/modules/ELS/Config.hpp
@@ -88,7 +88,7 @@ class Life_ELS {
         */
         class TCSD_D3S {
             profile = "d3s_default";
-            classNames[] = { "AG_CROWNVIC_Police_AG", "AG_CROWNVIC_Police_ST_AG", "AG_CROWNVIC_UNM_AG_TIER3", "AG_Bearcat_KCSO_AG", "AG_Titan_Police_AG" };
+            classNames[] = { "AG_CROWNVIC_Police_AG", "AG_CROWNVIC_Police_ST_AG", "AG_CROWNVIC_UNM_AG_TIER3", "AG_Bearcat_KCSO_AG", "AG_Titan_Police_AG", "AG_Camaro_21_HSU_AG_TIER3", "AG_CTSV_AdminServices_AG_TIER3", "AG_Bearcat_KCSO_AG", "AG_Titan_Police_AG" };
         };
     };
 };
